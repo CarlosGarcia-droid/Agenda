9275fb9d58fdd58122cc3358f0f0eb1e41cf89ba
GuillemPT <47145091+GuillemPT@users.noreply.github.com>
2020-09-21 15:26:30 +0200

    Update tables.sql

--- BBDD/tables.sql
+++ BBDD/tables.sql
@@ -1,1 +1,9 @@
-CREATE TABLE
+CREATE TABLE Usuario (
+  nombre VARCHAR(50) NOT NULL,
+  usuario VARCHAR(50) NOT NULL,
+  contraseña  VARCHAR(50) NOT NULL,
+  email VARCHAR(100) NOT NULL,
+  codigo VARCHAR(50) NOT NULL,
+  CONSTRAINT codigo PRIMARY KEY (codigo), --clave primaria
+  CONSTRAINT usuario UNIQUE (usuario),
+);
