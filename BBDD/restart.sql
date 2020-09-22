952df520c741c6f18deaa219c1e0b49f60422e3a
GuillemPT <47145091+GuillemPT@users.noreply.github.com>
2020-09-22 02:57:16 +0200

    Update restart.sql

--- BBDD/restart.sql
+++ BBDD/restart.sql
@@ -8,7 +8,7 @@ CREATE TABLE Usuario (
   nombre VARCHAR(50) NOT NULL,
   usuario VARCHAR(50) NOT NULL UNIQUE,
   contraseña  VARCHAR(50) NOT NULL,
-  email VARCHAR(100) NOT NULL,
+  email VARCHAR(100) NOT NULL UNIQUE,
   codigo VARCHAR(50) PRIMARY KEY
 );
 
